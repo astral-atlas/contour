93a0082baa6dc03ba88c344414625768e6273647
Luke Kaalim <lukekaalim@gmail.com>
2019-09-28 01:18:28 +1000

    Add location model

--- src/index.js
+++ src/index.js
@@ -2,18 +2,34 @@
 /*::
 import type { Model } from '@lukekaalim/model';
 */
-import { nameModel, stringModel, numberModel, modelObject } from '@lukekaalim/model';
+import { nameModel, stringModel, numberModel, modelObject, modelOptional } from '@lukekaalim/model';
 
 /*::
+export type LocationID = string;
+export type Location = {
+  id: LocationID,
+  name: string,
+};
+
+export type SessionID = string;
 export type Session = {
-  id: string,
+  id: SessionID,
   title: string,
   startTime: number,
+  startLocation: null | LocationID,
 };
 */
 
+export const locationIdModel/*: Model<LocationID>*/ = nameModel('LocationID', stringModel);
+export const locationModel/*: Model<Location>*/ = nameModel('Location', modelObject({
+  id: locationIdModel,
+  name: stringModel,
+}));
+
+export const seesionIdModel/*: Model<SessionID>*/ = nameModel('SessionID', stringModel);
 export const sessionModel/*: Model<Session>*/ = nameModel('Session', modelObject({
-  id: stringModel,
+  id: seesionIdModel,
   title: stringModel,
   startTime: numberModel,
+  startLocation: modelOptional(locationIdModel),
 }));
